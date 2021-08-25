89367dfdade9bc042f0ea6bc5be9bb4d8a42adeb
ReyanshM <72616867+ReyanshM@users.noreply.github.com>
2021-08-25 17:53:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-// Still no code
+function setup(){
+    canvas=createCanvas(600,600);
+    canvas.parent('game');
+    video=createCapture(VIDEO);
+    video.size(600,600);
+    video.parent('game');
+    posenet=ml5.poseNet(video, modelLoaded);
+}
+function reStart(){
+
+}
+function play(){
+    
+}
+function draw(){
+    
+}
+function modelLoaded(){
+    console.log("The is loaded");
+}
+function preload(){
+
+}
