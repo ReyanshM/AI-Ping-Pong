b98adba82289731d0925f3158e1a6a2800e844d7
ReyanshM <72616867+ReyanshM@users.noreply.github.com>
2021-09-01 10:56:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
+rightWristY=0;
+rightWristX=0;
+function preload(){
+
+}
 function setup(){
-    canvas=createCanvas(600,600);
+    canvas=createCanvas(500,500);
     canvas.parent('game');
     video=createCapture(VIDEO);
-    video.size(600,600);
+	video.size(500,500);
     video.parent('game');
+    video.hide();
     posenet=ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', results);
+}
+function results(results){
+    if(results.length>0){
+        rightWristY=results[0].pose.rightWrist.y;
+        rightWristX=results[0].pose.rightWrist.x;
+    }
 }
 function reStart(){
 
@@ -13,11 +26,11 @@ function play(){
     
 }
 function draw(){
-    
+    image(video,0,0,500,500);
+    fill('red');
+    stroke('red');
+    circle(30,rightWristY,20);
 }
 function modelLoaded(){
-    console.log("The is loaded");
-}
-function preload(){
-
+    console.log("The modal is loaded");
 }
